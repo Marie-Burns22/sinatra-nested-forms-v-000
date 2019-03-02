482e6392f021140253518d2f340aa1c04147dd2c
Marie Burns <mcburns2222@gmail.com>
2019-03-02 20:43:22 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,19 @@
 class Ship
-  attr_accessor :
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear 
+    @@all = []
+  end
+  
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
 end
