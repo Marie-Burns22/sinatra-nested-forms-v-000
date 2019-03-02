7463634089aa60c44070f708ca92d0c8e1dfdbd4
Marie Burns <mcburns2222@gmail.com>
2019-03-02 20:58:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get "/" do 
+      erb :new 
+    end
+    
+    post "/pirate"
+  
     # code other routes/actions here
 
   end
